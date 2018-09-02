9a767f0bcec7b5a6cab7bacc9713aa85d00fd216
GregorKoehler <gregorkoehler@web.de>
2018-09-02 19:51:05 +0200

    added v0 step() function and started usage example with policy gradients

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 A Pommerman Multi-Agent Reinforcement Learning Environment wrapped using RLlib's `MultiAgentEnv`.
 
 ### TODOs
-* [ ] implement v0.step()
-* [x] enable using different configs for each env version
+* [x] implement v0.step()
+* [x] enable using different configs, [ ] for each env version
--- envs/v0.py
+++ envs/v0.py
@@ -1,8 +1,50 @@
+import numpy as np
+
 from pommerman.envs.v0 import Pomme
 from pommerman.configs import ffa_competition_env
 from pommerman.configs import team_competition_env
 from pommerman.agents import SimpleAgent, RandomAgent, PlayerAgent, BaseAgent
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
+
+
+# constants
+NUM_PLAYERS = 4
+AGENT_IDS = ['agent_0', 'agent_1', 'agent_2', 'agent_3']
+
+def to_dict(list):
+    '''
+    Turn list values into dictionary where keys are the agent IDs.
+    '''
+    return {AGENT_IDS[i]: list[i] for i in range(NUM_PLAYERS)}
+
+def make_np_float(feature):
+    return np.array(feature).astype(np.float32)
+
+def featurize(obs):
+    board = obs["board"].reshape(-1).astype(np.float32)
+    bomb_blast_strength = obs["bomb_blast_strength"].reshape(-1).astype(np.float32)
+    bomb_life = obs["bomb_life"].reshape(-1).astype(np.float32)
+    position = make_np_float(obs["position"])
+    ammo = make_np_float([obs["ammo"]])
+    blast_strength = make_np_float([obs["blast_strength"]])
+    can_kick = make_np_float([obs["can_kick"]])
+
+    teammate = obs["teammate"]
+    if teammate is not None:
+        teammate = teammate.value
+    else:
+        teammate = -1
+    teammate = make_np_float([teammate])
+
+    enemies = obs["enemies"]
+    enemies = [e.value for e in enemies]
+    if len(enemies) < 3:
+        enemies = enemies + [-1] * (3 - len(enemies))
+    enemies = make_np_float(enemies)
+
+    features = np.concatenate(
+        (board, bomb_blast_strength, bomb_life, position, ammo, blast_strength, can_kick, teammate, enemies))
+    return features  # np.expand_dims(features, 1)
 
 
 class Pomme_v0(MultiAgentEnv):
@@ -37,16 +79,14 @@ class Pomme_v0(MultiAgentEnv):
         "__all__": False,
     }
     """
-    def __init__(self, training_agent_id=None, seed=1, config=team_competition_env()):
+    def __init__(self, config=team_competition_env()):
         self.pomme = Pomme(**config['env_kwargs'])
-        self.agent_names = ['agent_'+str(i) for i in range(4)]
+        self.agent_names = AGENT_IDS
         agent_list = []
         for i in range(4):
             agent_id = i
             agent_list.append(BaseAgent(config["agent"](agent_id, config["game_type"])))
         self.pomme.set_agents(agent_list)
-        #self.pomme.set_training_agent(training_agent_id)
-        self.pomme.seed(seed)
         self.pomme.set_init_game_state(None)
 
     def reset(self):
@@ -55,8 +95,7 @@ class Pomme_v0(MultiAgentEnv):
             obs (dict): New observations for each ready agent.
         """
         obs_list = self.pomme.reset()
-        obs = {self.agent_names[i]: obs_list[i] for i in range(4)}
-        return obs
+        return to_dict(obs_list)
 
     def step(self, action_dict):
         """Returns observations from ready agents.
@@ -71,10 +110,14 @@ class Pomme_v0(MultiAgentEnv):
                 "__all__" is used to indicate env termination.
             infos (dict): Info values for each ready agent.
         """
-        raise NotImplementedError
+        obs, rewards, done, info = self.pomme.step(list(action_dict.values()))
+        dones = {'__all__': done}
+        dones.update(to_dict([not agent.is_alive for agent in self.pomme._agents]))
+        return to_dict(obs), to_dict(rewards), dones, info
+
 
 if __name__ == '__main__':
-
+    action_dict = {'agent_0': 0, 'agent_1': 0, 'agent_2': 0, 'agent_3': 0}
     env = Pomme_v0()
     import IPython
     IPython.embed()
