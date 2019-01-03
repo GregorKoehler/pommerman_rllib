390b5f60c9aff66668c19c9e36f9fbe3b5666ae9
GregorKoehler <gregorkoehler@web.de>
2019-01-03 15:00:31 +0100

    update train example to separation of preprocessing and env wrapping

--- train_example.py
+++ train_example.py
@@ -1,16 +1,27 @@
+import atexit
 import ray
 from ray.tune.registry import register_env
 
 from envs.v0 import Pomme_v0
+from preprocessors.featurize_preprocessor import Featurize_Preprocessor
 from pommerman.configs import team_competition_env
 
+from ray.rllib.models import ModelCatalog
 from ray.rllib.agents.pg import PGAgent
 from ray.rllib.agents.pg.pg_policy_graph import PGPolicyGraph
+
+def shutdown():
+    print('Shutting down Ray...')
+    ray.shutdown()
+atexit.register(shutdown)
 
 
 if __name__ == '__main__':
     # register environment
     register_env('Pomme_v0', lambda config: Pomme_v0(config))
+    # register preprocessor
+    ModelCatalog.register_custom_preprocessor('Featurize_Preprocessor', Featurize_Preprocessor)
+
     ray.init()
 
     # get env config and create dummy instance to retrieve observation & action space
@@ -33,10 +44,14 @@ if __name__ == '__main__':
                               },
                               'policy_mapping_fn': lambda agent_id: agent_id
                           },
+                          'model': {
+                              'custom_preprocessor': 'Featurize_Preprocessor'
+                          },
                           'env_config': env_config
                       })
     print('\nTrainer Config:\n', trainer.config, '\n')
 
+    # quick IPython embed to review the config
     import IPython
     IPython.embed()
     while True:
